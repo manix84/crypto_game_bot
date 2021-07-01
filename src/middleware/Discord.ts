2001ada233e570a87bd752602f9ca38aa2ce4327
Rob Taylor <manix84@gmail.com>
2021-07-01 14:22:47 +0100

    Adding env vars for secret message.

--- src/middleware/Discord.ts
+++ src/middleware/Discord.ts
@@ -2,6 +2,8 @@ import Discord from "discord.js";
 import { success, info, br } from "../utils/log";
 
 const bot = new Discord.Client();
+const SECRET_CODE = process.env.SECRET_CODE || "test";
+const SECRET_REPLY = process.env.SECRET_REPLY || "https://www.youtube.com/watch?v=6-HUgzYPm9g";
 
 bot.on("ready", () => {
   info(`Logged in as ${bot.user?.tag}!`);
@@ -14,13 +16,13 @@ bot.on("message", (message: Discord.Message) => {
     message.channel.send("pong!");
   }
 
-  if (message.content === "1234") {
+  if (message.content === SECRET_CODE) {
     message.delete();
     success(`${message.author.username} successfully guessed the combination.`);
     const embeddedSetupMessage = new Discord.MessageEmbed()
       .setColor("#FFD700")
       .setTitle("Cryptic Clue")
-      .setDescription("Some text here.")
+      .setDescription(SECRET_REPLY)
       .setThumbnail(`https://${process.env.HOST}/images/logo.png`)
       .setTimestamp()
       .setFooter("Crypto Bot", `https://${process.env.HOST}/images/logo_bordered.png`);
